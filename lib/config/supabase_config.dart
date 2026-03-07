5de980a2b3e296a45158e741ebe5b807f8a0ff98
nouhaerr404 <nouhaerr05@gmail.com>
2026-03-07 15:06:57 +0000

    Structure Updated

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,9 @@
+class SupabaseConfig {
+
+  static const supabaseUrl =
+      "https://ltcmjsjkyxwbiszrcayr.supabase.co";
+
+  static const supabaseAnonKey =
+      "sb_publishable_xWAQePu4kNDwO6wumpWb6g_Zw3h75Gg";
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'config/supabase_config.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: SupabaseConfig.supabaseUrl,
+    anonKey: SupabaseConfig.supabaseAnonKey,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ResiManager',
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ResiManager")),
+      body: const Center(
+        child: Text("Supabase connecté avec succès"),
+      ),
+    );
+  }
+}
